f79afbb2d49a137e25bc04beffed601442b7da6b
simondyates <simon.yates@gmail.com>
2020-04-17 20:18:53 -0400

    Update to use href as identifier because it has detailed pareseable fields.

--- selenium_scrape.py
+++ selenium_scrape.py
@@ -2,34 +2,29 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import pandas as pd
-from pathlib import Path
-import re
+import csv
 
-def scrape_parker():
 #Use selenium to scrape the robertparker.com vintages table
-    driver = webdriver.Chrome()
-    driver.get("https://www.robertparker.com/resources/vintage-chart")
 
-    rows = WebDriverWait(driver, 10).until(
-                           EC.presence_of_all_elements_located((By.XPATH, '//table[@class="vintage-scores draggable"]//a')))
-    wine_list = []
-    for row in rows:
-        wine = row.get_attribute('title')
-        print(wine)
-        wine_list.append([wine , row.text])
+# Open a csv file to store the output
+csv_file = open('robertparker_temp.csv', 'w', encoding='utf-8', newline='')
+writer = csv.writer(csv_file)
+writer.writerow(['href', 'rating'])
 
-    wine_df = pd.DataFrame(wine_list, columns=['identifier', 'rating'])
-    wine_df.to_csv('parkervintages.csv', index=False)
-    driver.close()
+# Query the table rows
+driver = webdriver.Chrome()
+driver.get("https://www.robertparker.com/resources/vintage-chart")
+rows = WebDriverWait(driver, 10).until(
+                     EC.presence_of_all_elements_located((By.XPATH, '//table[@class="vintage-scores draggable"]//a')))
 
-# If we haven't already, run the web scraping function above
-if not Path('parkervintages.csv').exists():
-    scrape_parker()
+# Process each row returned
+for row in rows:
+    wine = row.get_attribute('href')
+    # we capture the href not because we want to access the page, but instead
+    # because it gives a logically-structured description of the wine that will be parseable
+    print(wine) # to keep track of the (slow) progress
+    writer.writerow([wine, row.text])
 
-# Use regex to parse the two fields
-wine_df = pd.read_csv('parkervintages.csv')
-wine_df['identifier'] = wine_df['identifier'].apply(str.replace, args=('\n', ' '))
-
-def parse_identifier(s):
-    re.search('(....) ()')
+# Clean up
+csv_file.close()
+driver.close()
